ecd44d1579c6c6123a073f98b30947d66b23f9fb
T0nci <138928016+T0nci@users.noreply.github.com>
2024-05-08 15:18:36 +0200

    Setup babel-loader for ES6 modules
    Since I had already installed babel and a babel-preset the only addition
    needed was to webpack common config.

--- webpack.common.js
+++ webpack.common.js
@@ -25,6 +25,16 @@ module.exports = {
         test: /\.(png|jpg|jpeg|svg|gif)/i,
         type: "asset/resource",
       },
+      {
+        test: /\.(?:js|mjs|cjs)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: [["@babel/preset-env", { targets: "defaults" }]],
+          },
+        },
+      },
     ],
   },
 };
