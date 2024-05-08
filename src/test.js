d2849d5225dbb8e010a0cb88db19332ea5d144a8
T0nci <138928016+T0nci@users.noreply.github.com>
2024-05-08 14:32:37 +0200

    Setup prettier

---
+++ src/test.js
@@ -0,0 +1,7 @@
+// From 'foo(reallyLongArg(), omgSoManyParameters(), IShouldRefactorThis(), isThereSeriouslyAnotherOne());' to this:
+foo(
+  reallyLongArg(),
+  omgSoManyParameters(),
+  IShouldRefactorThis(),
+  isThereSeriouslyAnotherOne(),
+);
