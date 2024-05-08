2c201fea3857a7e62e9b753b594afabc5e43cc31
T0nci <138928016+T0nci@users.noreply.github.com>
2024-05-08 14:12:29 +0200

    Setup eslint

---
+++ eslint.config.mjs
@@ -0,0 +1,16 @@
+import globals from "globals";
+import pluginJs from "@eslint/js";
+
+
+export default [
+  pluginJs.configs.recommended,
+  {
+    languageOptions: { 
+      globals: {...globals.browser, ...globals.node}
+    },
+    rules: {
+      "no-console": "off",
+    },
+    files: ["src/**/*.js"],
+  },
+];
