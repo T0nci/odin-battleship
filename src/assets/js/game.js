11aeb5bcbcc05da32688c8704f5ed48066f540c4
T0nci <138928016+T0nci@users.noreply.github.com>
2024-05-14 21:16:11 +0200

    feat: Refactor the code to support 2 player mode

--- src/assets/js/game.js
+++ src/assets/js/game.js
@@ -1,7 +1,5 @@
 import "./../css/style.css";
 
-import Ship from "./ship";
-import Gameboard from "./gameboard";
 import Player from "./player";
 import { Computer } from "./player";
 
@@ -72,16 +70,16 @@ function playComputerTurn(gameState, player, computer) {
   DOM.renderBoards(player.gameboard.gameboard, computer.gameboard.gameboard);
 }
 
-function playTurn(coordinates, gameState, player, computer) {
-  if (gameState.turn !== player) return;
+function playTurn(coordinates, gameState, player1, player2) {
+  if (gameState.turn !== player1) return;
 
-  const attack = computer.gameboard.receiveAttack(coordinates);
+  const attack = player2.gameboard.receiveAttack(coordinates);
   const options = {};
 
   if (attack === null) return;
   else if (attack === true) {
     const ship =
-      computer.gameboard.gameboard[coordinates[0]][coordinates[1]].ship;
+      player2.gameboard.gameboard[coordinates[0]][coordinates[1]].ship;
 
     if (ship.isSunk()) {
       options.sunk = ship.name;
@@ -89,31 +87,59 @@ function playTurn(coordinates, gameState, player, computer) {
   }
 
   // If player won
-  if (checkIfLost(computer)) {
-    DOM.endGame(player, computer, options);
+  if (checkIfLost(player2)) {
+    DOM.endGame(player1, player2, options);
     return;
   }
 
   // If it's a miss then pass in empty options
-  DOM.changeGameState(player, computer, options);
-  gameState.turn = computer;
-  DOM.renderBoards(player.gameboard.gameboard, computer.gameboard.gameboard);
+  DOM.changeGameState(player1, player2, options);
+  DOM.renderBoards(player1.gameboard.gameboard, player2.gameboard.gameboard);
 
-  setTimeout(() => {
-    playComputerTurn(gameState, player, computer);
-  }, 2 * 1000);
+  // If the second player is a computer
+  if (player2 instanceof Computer) {
+    gameState.turn = player2;
+
+    setTimeout(() => {
+      playComputerTurn(gameState, player1, player2);
+    }, 2 * 1000);
+  } else {
+    // Change the state to null so the current player can't play multiple times
+    gameState.turn = null;
+
+    setTimeout(() => {
+      // Moved this here cuz switching the turn too early(before the callback
+      // from the other addEventListener) causes some not needed side-effects -
+      // the game messes up by playing 2 turns wrongfully in multiplayer mode.
+      // Changing it after the second player's callback as a setTimeout
+      // callback is needed.
+      gameState.turn = player2;
+
+      DOM.switchScreen();
+      DOM.renderBoards(
+        player2.gameboard.gameboard,
+        player1.gameboard.gameboard,
+      );
+
+      setTimeout(() => {
+        DOM.switchScreen();
+      }, 5 * 1000);
+    }, 3 * 1000);
+  }
 }
 
-function playGame() {
-  const player = new Player("1");
-  const computer = new Computer("Computer");
+function playGame(mode) {
+  const player1 = new Player("Player 1");
+  let player2;
+  if (mode === "multiplayer") player2 = new Player("Player 2");
+  else player2 = new Computer("Computer");
 
-  DOM.renderBoards(player.gameboard.gameboard, computer.gameboard.gameboard);
+  DOM.renderBoards(player1.gameboard.gameboard, player2.gameboard.gameboard);
 
-  const gameState = { turn: player };
+  const gameState = { turn: player1 };
 
   document.querySelector(".game-state > .turn").textContent =
-    `${player.name}'s Turn`;
+    `${player1.name}'s Turn`;
 
   document
     .querySelector(".place-ships > .randomize")
@@ -122,9 +148,18 @@ function playGame() {
       // when creating 2 player mode
       gameState.turn.gameboard.randomizeShips();
 
+      let tempPlayer1, tempPlayer2;
+      if (player1 === gameState.turn) {
+        tempPlayer1 = player1;
+        tempPlayer2 = player2;
+      } else {
+        tempPlayer1 = player2;
+        tempPlayer2 = player1;
+      }
+
       DOM.renderBoards(
-        player.gameboard.gameboard,
-        computer.gameboard.gameboard,
+        tempPlayer1.gameboard.gameboard,
+        tempPlayer2.gameboard.gameboard,
       );
     });
 
@@ -132,35 +167,97 @@ function playGame() {
     .querySelector(".place-ships > .ready")
     .addEventListener("click", (e) => {
       // Disable the buttons so the user can't interfere
-      const ready = e.currentTarget;
-      const randomize = ready.parentNode.querySelector(".randomize");
-      ready.disabled = true;
-      randomize.disabled = true;
-      ready.classList.add("disabled", "clicked");
-      randomize.classList.add("disabled");
+      DOM.switchReadyButton("disable");
 
-      computer.gameboard.randomizeShips();
+      // If the second player is a computer initiate the game
+      if (player2 instanceof Computer) {
+        player2.gameboard.randomizeShips();
 
-      DOM.renderBoards(
-        player.gameboard.gameboard,
-        computer.gameboard.gameboard,
-      );
+        DOM.renderBoards(
+          player1.gameboard.gameboard,
+          player2.gameboard.gameboard,
+        );
 
-      document
-        .querySelector(".boards > .board-space > .enemy-board")
-        .addEventListener("click", (e) => {
-          // Makes sure the target is a cell and not a border or gap
-          if (!e.target.classList.contains("cell")) return;
+        document
+          .querySelector(".boards > .board-space > .enemy-board")
+          .addEventListener("click", (e) => {
+            // Makes sure the target is a cell and not a border or gap
+            if (!e.target.classList.contains("cell")) return;
 
-          playTurn(
-            e.target.dataset.coordinates
-              .split(",")
-              .map((value) => parseInt(value)),
-            gameState,
-            player,
-            computer,
-          );
-        });
+            playTurn(
+              e.target.dataset.coordinates
+                .split(",")
+                .map((value) => parseInt(value)),
+              gameState,
+              player1,
+              player2,
+            );
+          });
+      } else if (player2.gameboard.areAllShipsSunk()) {
+        // Else if it's another player that's not ready, enable ready for him
+        gameState.turn = player2;
+        document.querySelector(".game-state > .turn").textContent =
+          `${player2.name}'s Turn`;
+
+        DOM.switchScreen();
+        DOM.renderBoards(
+          player2.gameboard.gameboard,
+          player1.gameboard.gameboard,
+        );
+
+        setTimeout(() => {
+          DOM.switchScreen();
+          DOM.switchReadyButton("enable");
+        }, 5 * 1000);
+      } else {
+        // Else both players are ready and we can initiate the game
+        gameState.turn = player1;
+        document.querySelector(".game-state > .turn").textContent =
+          `${player1.name}'s Turn`;
+
+        DOM.switchScreen();
+        DOM.renderBoards(
+          player1.gameboard.gameboard,
+          player2.gameboard.gameboard,
+        );
+
+        setTimeout(() => {
+          DOM.switchScreen();
+        }, 5 * 1000);
+
+        // only difference between the 2 is the places of players
+        document
+          .querySelector(".boards > .board-space > .enemy-board")
+          .addEventListener("click", (e) => {
+            // Makes sure the target is a cell and not a border or gap
+            if (!e.target.classList.contains("cell")) return;
+
+            playTurn(
+              e.target.dataset.coordinates
+                .split(",")
+                .map((value) => parseInt(value)),
+              gameState,
+              player1,
+              player2,
+            );
+          });
+
+        document
+          .querySelector(".boards > .board-space > .enemy-board")
+          .addEventListener("click", (e) => {
+            // Makes sure the target is a cell and not a border or gap
+            if (!e.target.classList.contains("cell")) return;
+
+            playTurn(
+              e.target.dataset.coordinates
+                .split(",")
+                .map((value) => parseInt(value)),
+              gameState,
+              player2,
+              player1,
+            );
+          });
+      }
     });
 }
 
