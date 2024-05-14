0718024374b1ff8879db7b4270cf071ed934722c
T0nci <138928016+T0nci@users.noreply.github.com>
2024-05-14 20:43:15 +0200

    feat: Complete the choosing mode dialog

--- src/index.js
+++ src/index.js
@@ -1,3 +1,22 @@
 import playGame from "./assets/js/game";
 
-playGame();
+// Cancel closing of dialogs because they are mandatory
+document.querySelector("html").addEventListener("keydown", (e) => {
+  if (e.key === "Escape") e.preventDefault();
+});
+
+document.querySelector(".choose-mode").showModal();
+document
+  .querySelector(".choose-mode > form")
+  .addEventListener("submit", (e) => {
+    e.preventDefault();
+
+    // e.target is the form
+    const mode = e.target.querySelector("input[type='radio']:checked").value;
+
+    // Close the dialog and reset the form
+    e.target.parentNode.close();
+    e.target.reset();
+
+    playGame(mode);
+  });
